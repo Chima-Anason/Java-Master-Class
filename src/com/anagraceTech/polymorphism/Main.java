9754732101273ff7ed1fe785d70c77d2393b242e
Chima-Anason <tblynxx@gmail.com>
2023-04-18 14:08:02 +0800

    Constants and Default methods in interface, Closes #7

--- src/com/anagraceTech/polymorphism/Main.java
+++ src/com/anagraceTech/polymorphism/Main.java
@@ -28,5 +28,7 @@ public class Main {
             System.out.println();
         }
 
+        System.out.println(car.milesToKm());
+
     }
 }
--- src/com/anagraceTech/polymorphism/Vehicles.java
+++ src/com/anagraceTech/polymorphism/Vehicles.java
@@ -3,13 +3,19 @@ package com.anagraceTech.polymorphism;
 public interface Vehicles {
 
     //interfaces can have the following:
-        // constants
-        // abstract methods
+
+    //1. constants
+    double PURCHASE_RATE = 0.5;
+
+    //2. abstract methods
     void move(int amount);
 
     void applyBreaks(int amount);
 
     int getCurrentSpeed();
 
-        // default methods
+    // default methods
+    default double milesToKm(){
+        return getCurrentSpeed()* 1.609;
+    }
 }
